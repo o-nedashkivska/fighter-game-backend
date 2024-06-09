953fa07382f6c79172a78f95c7a1e5f0c88b7f59
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-06-10 02:06:29 +0300

    add fighterRoutes

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -8,6 +8,114 @@ import {
 
 const router = Router();
 
-// TODO: Implement route controllers for fighter
+router.get(
+  "/",
+  (req, res, next) => {
+    try {
+      const fighters = fighterService.getFighters();
+
+      res.data = fighters;
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const fighter = fighterService.getFighterById(id);
+
+      if (fighter) {
+        res.data = fighter;
+        res.status(200);
+      } else {
+        res.err = "Fighter not found";
+        res.status(404);
+      }
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.post(
+  "/",
+  createFighterValid,
+  (req, res, next) => {
+    try {
+      if (!res.err) {
+        const data = req.body;
+        const fighter = fighterService.addFighter(data);
+
+        res.data = fighter;
+        res.status(200);
+      }
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.patch(
+  "/:id",
+  updateFighterValid,
+  (req, res, next) => {
+    try {
+      if (!res.err) {
+        const { id } = req.params;
+        const data = req.body;
+        const fighter = fighterService.updateFighterById(id, data);
+
+        if (fighter) {
+          res.data = fighter;
+          res.status(200);
+        } else {
+          res.err = "Fighter not found";
+          res.status(404);
+        }
+      }
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const fighter = fighterService.deleteFighterById(id);
+
+      if (fighter) {
+        res.data = fighter;
+        res.status(200);
+      } else {
+        res.err = "Fighter not found";
+        res.status(404);
+      }
+    } catch (err) {
+      res.err = err;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
 
 export { router };
