419d7e997cea53c8f30c97d64cf11fdf3b0459f1
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-06-10 02:00:34 +0300

    add fighterService methods

--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,50 @@
 import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FighterService {
-  // TODO: Implement methods to work with fighters
+  getFighters() {
+    return fighterRepository.getAll();
+  }
+
+  getFighterById(id) {
+    const search = (fighter) => fighter.id === id;
+    const item = fighterRepository.getOne(search);
+
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+
+  addFighter(data) {
+    if (!data.health) {
+      data.health = 85;
+    }
+
+    const item = fighterRepository.create(data);
+
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+
+  updateFighterById(id, data) {
+    const item = this.getFighterById(id);
+
+    if (!item) {
+      return null;
+    }
+    return fighterRepository.update(id, data);
+  }
+
+  deleteFighterById(id) {
+    const item = fighterRepository.delete(id);
+
+    if (!item) {
+      return null;
+    }
+    return item[0];
+  }
 }
 
 const fighterService = new FighterService();
