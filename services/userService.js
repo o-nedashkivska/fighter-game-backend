344fdb3f934259bf720a1bc919d2071bac548e59
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-06-10 17:20:33 +0300

    add userService methods

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,9 @@
 import { userRepository } from "../repositories/userRepository.js";
 
 class UserService {
-  // TODO: Implement methods to work with user
+  getUsers() {
+    return userRepository.getAll();
+  }
 
   search(search) {
     const item = userRepository.getOne(search);
@@ -9,6 +11,54 @@ class UserService {
       return null;
     }
     return item;
+  }
+
+  getUserById(id) {
+    const search = (user) => user.id === id;
+    const item = this.search(search);
+
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+
+  getUserByProperties(properties) {
+    const search = (user) =>
+      Object.entries(properties).every(([key, value]) => user[key] === value);
+    const item = this.search(search);
+
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+
+  addUser(data) {
+    const item = userRepository.create(data);
+
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
+
+  updateUserById(id, data) {
+    const item = this.getUserById(id);
+
+    if (!item) {
+      return null;
+    }
+    return userRepository.update(id, data);
+  }
+
+  deleteUserById(id) {
+    const item = userRepository.delete(id);
+
+    if (!item) {
+      return null;
+    }
+    return item[0];
   }
 }
 
